9fbc3d75c59f875f3cf57540e6965456a9b1dfe1
Samuel Huang <lspss95207@gmail.com>
2022-09-16 15:50:32 +0800

    Lab1: Add zip.sh

---
+++ Lab1/zip.sh
@@ -0,0 +1,10 @@
+rm -rf project1_311511034
+rm -rf project1_311511034.zip
+
+mkdir project1_311511034
+cp project1_part2_311511034.py project1_311511034
+cp project1_part3_311511034.py project1_311511034
+cp report/project1_311511034.pdf project1_311511034/project1_311511034.pdf
+
+zip -r project1_311511034.zip project1_311511034
+rm -rf project1_311511034
