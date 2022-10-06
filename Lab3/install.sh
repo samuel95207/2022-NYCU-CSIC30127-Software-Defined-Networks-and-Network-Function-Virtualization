9d8f85d05615355028185c1dca74b40be4620caf
Samuel Huang <lspss95207@gmail.com>
2022-10-06 17:04:04 +0800

    Lab3: Change app name to required format

--- Lab3/install.sh
+++ Lab3/install.sh
@@ -1,3 +1,3 @@
-onos localhost app deactivate nctu.winlab.bridge
+onos-app localhost deactivate nctu.winlab.bridge
 onos-app localhost uninstall nctu.winlab.bridge
-onos-app localhost install! bridge-app/target/bridge-app-1.0-SNAPSHOT.oar
+onos-app localhost install! bridge-app/target/bridge-034-1.0-SNAPSHOT.oar
