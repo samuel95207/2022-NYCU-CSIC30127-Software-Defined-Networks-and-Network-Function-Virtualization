fb4166cc06e848e95baec4d7d09708d402eddf52
Samuel Huang <lspss95207@gmail.com>
2022-11-08 02:52:18 +0800

    Lab5: Add zip.sh

---
+++ Lab5/zip.sh
@@ -0,0 +1,10 @@
+rm -rf project5_311511034
+rm -rf project5_311511034.zip
+
+mkdir project5_311511034
+
+cp -r ProxyArp/* project5_311511034
+rm -rf project5_311511034/target
+
+zip -r project5_311511034.zip project5_311511034
+rm -rf project5_311511034
